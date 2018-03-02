35c04cd68bc29b453898c32e22a7e26f93e9ee21
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-02 16:04:23 +0530

    Create IOSGRAM.C
    print the number

---
+++ IOSGRAM.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<conoi.h>
+int main()
+{
+    int i,flag;
+char a[100];
+printf("enter the string");
+scanf("%s",a);
+for(i=0;i<strlen(a);i++)
+{
+a[i]=tolower(a[i]);
+}
+for(i=0;i<strlen(a);i++)
+{
+if(a[i]==a[i+1])
+{
+    flag=1;
+    break;
+}
+}
+if(flag==1)
+printf("FALSE");
+else
+printf("true");
+return 0;
+}
+© 2018 GitHu
